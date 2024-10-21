94ebafd5fb842a52e1225192910befc9921d3033
jonatan <jonatansalva.js14@gmail.com>
2024-10-21 00:38:01 -0300

    Se agregaron operaciones en los Repositorys

--- src/main/java/com/client/manager/repository/ClientRepository.java
+++ src/main/java/com/client/manager/repository/ClientRepository.java
@@ -3,6 +3,18 @@ package com.client.manager.repository;
 import com.client.manager.model.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClientRepository extends JpaRepository <Client,Long> {
+
+    Integer countByPhone (String phone);
+
+    Integer countByEmail (String email);
+
+    List<Client> findClientByEmail(String email);
+
+    List<Client> findClientByName(String name);
+
+    List<Client> findClientByLastName(String lastName);
 
 }
